91ee12aee59b37e5df29a24e21d52fd0a195b1d8
Marshall <tum92783@temple.edu>
2022-04-25 23:52:49 -0400

    clicking on a circle shows a bar graph now

--- floodplains.js
+++ floodplains.js
@@ -1,16 +1,18 @@
 (function() {
-    var width = 1200, height = 800
-
+    var width = 1200, height = 800;
+    var barWidth = 550, barHeight=350;
     Promise.all([
         d3.json("./data/floodplains/philadelphia.geojson"),
         d3.json("./data/floodplains/FEMA_100_Flood_Plain2.geojson"),
         d3.json("./data/floodplains/FEMA_500_Flood_Plain2.geojson"),
-        d3.csv("./data/floodplains/floodcounts.csv")
+        d3.csv("./data/floodplains/floodcounts.csv"),
+        d3.csv("./data/floodplains/yearly_floodcounts.csv")
     ]).then((data) => {
         const philadelphia = data[0];
         const floodplains100 = data[1];
         const floodplains500 = data[2];
         const floods = data[3]
+        const yearlyFloods = data[4]
         let mapProjection = d3.geoMercator()
         .fitExtent([[0, 0], [width, height]], philadelphia);
         
@@ -28,6 +30,38 @@
             floodDict.set(location.location, +location.flood_count)
         })
 
+        var yearlyFloodData = new Map();
+        yearlyFloods.forEach((location) => {
+            yearlyFloodData.set(location.location, new Map([
+                ["2006", location[2006]],
+                ["2007", location[2007]],
+                ["2008", location[2008]],
+                ["2009", location[2009]],
+                ["2010", location[2010]],
+                ["2011", location[2011]],
+                ["2012", location[2012]],
+                ["2013", location[2013]],
+                ["2014", location[2014]],
+                ["2015", location[2015]],
+                ["2016", location[2016]],
+                ["2017", location[2017]],
+                ["2018", location[2018]],
+            ]
+            ))
+        });
+       const distinctValues = Array.from(yearlyFloodData.get("Airport").keys());
+       // console.log(yearlyFloodData.get("Airport"))
+       console.log(distinctValues);
+        var x = d3.scaleBand()
+        .domain(distinctValues)
+        .range([20, barWidth])
+        .padding(0.1);
+
+        var y = d3.scaleLinear()
+        .domain([0, 4])
+        .range([barHeight, 50]);
+
+       // console.log(yearlyFloodData)
         var blues = d3.scaleSequential()
         .domain(d3.extent(floodDict.values()))
         .range(['#deebf7', '#3182bd'])
@@ -38,6 +72,9 @@
         .range([5, 15]);
 
         const svg = d3.select("#floodplains").append('g').attr('transform', 'translate(50,50)');
+        const bar1 = d3.select("#bar1").append("g").attr('transform', 'translate(50,0)');
+        const bar2 = d3.select("#bar2").append("g").attr('transform', 'translate(50,0)');
+        var bar1Set = false;
 
         let map = svg.append('g')
         .selectAll("path")
@@ -67,12 +104,16 @@
         })
         .attr("r", (d) => {
             if (floodDict.has(d.properties.name)) {
-                console.log(d)
+                //console.log(d)
                 return scaleCircles(floodDict.get(d.properties.name))
             }
             else return 0;
         })
-        .attr("fill", "#0c407c");
+        .attr("fill", "#0c407c")
+        .on('click', (event, d) => {
+            console.log(d)
+           drawBars(d.properties.name);
+        });
 
         let map2 = svg.append('g')
         .selectAll("path")
@@ -100,6 +141,90 @@
         .attr("stroke", "4f8fbc")
         .attr("stroke-width", "0px");
         
+        function drawBars(location) {
+            data = yearlyFloodData.get(location);
+            var data = Array.from(data, ([year, value]) => ({ year, value }));
+            console.log(data);
+           // if (!bar1Set) {
+                bar1.selectAll('rect').remove();
+                var bg = bar1.append('rect')
+                .attr("x", -30)
+                .attr("y", 5)
+                .attr("width", 600)
+                .attr("height", 380)
+                .attr("fill", "#dddddd")
+                .attr("stoke-width", "1px")
+                .attr('stroke', "black");
+
+                bars = bar1.append('g')
+                .selectAll('rect')
+                .data(data)
+                .join('rect')
+                .attr("x", (d) => {
+                    return x(d.year);
+                })
+                .attr("y", (d) => {
+                    //console.log(y(d.value))
+                    return y(d.value);
+                })
+                .attr("width", "35px")
+                .attr("height", function(d) { 
+                // console.log(y(+d.value))
+                    return barHeight - y(+d.value); })
+                .attr("fill", "#0c407c");
+                
+                bar1.append('text') 
+                .attr("y", 25)
+                .attr("x", barWidth/2 - 25)
+                .text(location);
+
+                bar1.append('text')
+                .attr('class', 'axis-title') 
+                .attr("x", barWidth/2)
+                .attr("y", barHeight + 30)
+
+                .text('Year');
+
+                bar1.append('text')
+                .attr('class', 'axis-title') 
+                .attr("y", -15)
+                .attr("x", -((barHeight/2)+25))
+                .style("text-anchor", "middle")
+                .attr("transform", "rotate(-90)")
+                .text('Floods');
+
+                bar1.append("g")
+                .attr("transform", "translate(0," + (barHeight) + ")")
+                .call(
+                    d3.axisBottom(x)
+                );
+                bar1.append("g")
+                .attr("transform", "translate(15, 0)")
+                .call(
+                    d3.axisLeft(y).ticks(4)
+                );
+           // }
+/*             else {
+                bar2.selectAll('rect').remove()
+                bars = bar2.append('g')
+                .selectAll('rect')
+                .data(data)
+                .join('rect')
+                .attr("x", (d) => {
+                    return x(d.year);
+                })
+                .attr("y", (d) => {
+                    //console.log(y(d.value))
+                    return y(d.value);
+                })
+                .attr("width", "35px")
+                .attr("height", function(d) { 
+                // console.log(y(+d.value))
+                    return barHeight - y(+d.value); })
+                .attr("fill", "#0c407c");
+            } */
+        }
+
         function updateFloodplain(year) {
            if (year == 100) {
                map2
